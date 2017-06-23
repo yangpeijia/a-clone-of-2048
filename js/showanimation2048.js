65111bf891ccdda42cf02b4baa3cc919ffa26b38
yangpeijia <zxzy1g@163.com>
2017-06-23 21:40:31 +0800

    Add files via upload

--- js/showanimation2048.js
+++ js/showanimation2048.js
@@ -1,27 +1,26 @@
+function showNumberWithAnimation(i, j, randNumber) {
+    var the_number_cell = $("#number-cell-" + i + "-" + j);
+    the_number_cell.css('background-color', getNumberBackgroundColor(randNumber));
+    the_number_cell.css('color', getNumberColor(randNumber));
+    the_number_cell.text(randNumber);
 
-function showNumberWithAnimation(i,j,randNumber){
-    var the_number_cell=$("#number-cell-"+i+"-"+j);
-    the_number_cell.css('background-color',getNumberBackgroundColor( randNumber ) );
-    the_number_cell.css('color',getNumberColor( randNumber ) );
-    the_number_cell.text( randNumber );
-    
     the_number_cell.animate({
-        width:cell_side_length,
-        height:cell_side_length,
-        left:getPosLeft(i,j),
-        top:getPosTop(i,j)
-    },50)
-    
+        width: cell_side_length,
+        height: cell_side_length,
+        left: getPosLeft(i, j),
+        top: getPosTop(i, j)
+    }, 50)
+
 }
 
-function showMoveAnimation(fromx,fromy,tox,toy){
-    var the_number_cell=$("#number-cell-"+fromx+"-"+fromy);
+function showMoveAnimation(fromx, fromy, tox, toy) {
+    var the_number_cell = $("#number-cell-" + fromx + "-" + fromy);
     the_number_cell.animate({
-        top:getPosTop(tox,toy),
-        left:getPosLeft(tox,toy)
-    },200);
+        top: getPosTop(tox, toy),
+        left: getPosLeft(tox, toy)
+    }, 100);
 }
 
-function updateScore(score){
+function updateScore(score) {
     $("#score").text(score);
 }
